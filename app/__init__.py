2f9afea96f9e0c3eedebf8dea704fc87b3a8acb2
LeeAnn-Mokoena <leeann.mokoena@gmail.com>
2023-10-15 17:54:57 -0400

    setup for user login

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    from .main import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
