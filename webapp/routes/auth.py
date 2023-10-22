19f7eb5d885c33ef69024ae4924dbca0280dec8c
LeeAnn-Mokoena <leeann.mokoena@gmail.com>
2023-10-22 18:32:19 -0400

    for-admin

--- webapp/routes/auth.py
+++ webapp/routes/auth.py
@@ -1,17 +1,18 @@
-#from  werkzeug.security import generate_password_hash
-from flask import Blueprint, g, render_template, request, jsonify, flash, url_for
+from flask import Blueprint, g, render_template, request, jsonify, flash, url_for, redirect
 from pymongo import MongoClient
 import os
+import json
 from dotenv import load_dotenv
 from passageidentity import Passage, PassageError
-import jwt
-#import pprint
-#from bson.objectid import ObjectId
+from bson.objectid import ObjectId
 
 load_dotenv()
 
 auth = Blueprint('auth', __name__)
 main = Blueprint('main', __name__)
+dashboard = Blueprint('dashboard', __name__)
+
+mongo_client = MongoClient(os.getenv("MONGO_URI"))
 
 
 API_URL = os.getenv("API_URL")
@@ -26,34 +27,74 @@ except PassageError as e:
     print(e)
     exit()
 
-
-"""@auth.before_request
+@auth.before_request
 def before_request():
     try:
-        g.user = psg.authenticateRequest(request)
-        print("g user", g.user)
-    except PassageError as e:
-        print(f"PassageError: {str(e)}")
-        cookies = request.cookies
-        print("request headers", cookies)
-        return render_template('unauthorized.html')"""
+        if (request.path.startswith('/admin') or request.path.startswith('/register')):
+            g.admin_request = True
+            g.user = psg.authenticateRequest(request)
+            return render_template('index.html', psg_app_id=PASSAGE_APP_ID)
+        else:
+            g.admin_request = False
+    except PassageError:
+        return render_template('unauthorized.html')
+    
+
+@dashboard.route('/', methods=['GET'])
+def dashboard_main():
+    #fetch volunteer opportunities
+    organizations_list = mongo_client.volunteer_connect.organization.find()
+    existing_opportunities = []
+    for org in organizations_list:
+        v_opportunity_list = org.get("volunteerOpportunities")
+        existing_opportunities.append(v_opportunity_list)
+    all_volunteer_opportunities = process_opportunities(existing_opportunities)
+    return render_template('home.html', opportunities=all_volunteer_opportunities)
     
 @auth.route('/register')
 def register():
     return render_template('register.html', psg_app_id=PASSAGE_APP_ID)
 
-"""@main.route('/')
-def index():
-    return render_template('index.html', psg_app_id=PASSAGE_APP_ID)"""
+@auth.route('/admin/register-opportunity', methods=['POST', 'GET'])
+def volunteer_opportunities():
+    if(request.method == 'POST' and g.admin_requst == True):
+        organization_id = request.form.get('organizationId')
+        title = request.form.get('title')
+        description = request.form.get('description')
+        location = request.form.get('location')
+        date = request.form.get('date')
+        time = request.form.get('time')
 
-@auth.route('/dashboard', methods=['GET'])
-def dashboard():
-    psg_user = psg.getUser(g.user)
-    print("psg user", psg_user)
+        existing_org_id = ObjectId(organization_id)
+        existing_org = mongo_client.volunteer_opportunities.volunteering_options.find_one({"_id": existing_org_id})
 
-    identifier = ""
-    if psg_user.email:
-        identifier = psg_user.emaill
-    elif psg_user.phone:
-        identifier = psg_user.phone
-    return render_template('dashboard.html', psg_app_id=PASSAGE_APP_ID)
+        if(existing_org == None):
+            flash("You cannot add a volunteer option for an organization that is not registered", category='error')
+
+        else:
+            volunteer_id = mongo_client.volunteer_opportunities.volunteering_options.insert_one(
+                {
+                    'organization_id': organization_id,
+                    'title': title,
+                    'description': description,
+                    'location': location,
+                    'date': date,
+                    'time': time
+                }
+            ).inserted_id
+        if(volunteer_id != None):
+            flash("Submitted Successfully. Thank you for your submittion!", category='success')
+            return redirect(url_for(views.home))
+    else:
+        render_template('unauthorized.html')
+    return render_template("volunteer_opportunities.html")
+
+#helper function section
+def process_opportunities(existing_opportunities):
+    results = []
+    for opportunity_list in existing_opportunities:
+        for json_str_dict in opportunity_list:
+            json_string = json.dumps(json_str_dict)
+            results.extend([json.loads(json_string)])
+    #pprint.pprint(results)
+    return results
