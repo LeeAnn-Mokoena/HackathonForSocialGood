e94fc7bc1c257099d228b7ffd7000036217e7a20
LeeAnn-Mokoena <leeann.mokoena@gmail.com>
2023-10-23 19:57:02 -0400

    working user registeration

--- webapp/routes/auth.py
+++ webapp/routes/auth.py
@@ -1,18 +1,16 @@
 from flask import Blueprint, g, render_template, request, jsonify, flash, url_for, redirect
 from pymongo import MongoClient
 import os
-import json
-import pprint
 from dotenv import load_dotenv
 from passageidentity import Passage, PassageError
 from bson.objectid import ObjectId
-from datetime import datetime
+
 
 load_dotenv()
 
 auth = Blueprint('auth', __name__)
 main = Blueprint('main', __name__)
-dashboard = Blueprint('dashboard', __name__)
+#dashboard = Blueprint('dashboard', __name__)
 
 mongo_client = MongoClient(os.getenv("MONGO_URI"))
 API_URL = os.getenv("API_URL")
@@ -35,51 +33,7 @@ def before_request():
             g.admin_request = False
     except PassageError:
         return render_template('unauthorized.html')
-    
 
-@dashboard.route('/', methods=['GET'])
-def dashboard_main():
-    #fetch volunteer opportunities
-    organizations_list = mongo_client.volunteer_connect.organization.find()
-    existing_opportunities = []
-    for org in organizations_list:
-        v_opportunity_list = org.get("volunteerOpportunities")
-        existing_opportunities.append(v_opportunity_list)
-    all_volunteer_opportunities = process_opportunities(existing_opportunities)
-    return render_template('home.html', opportunities=all_volunteer_opportunities)
-
-@dashboard.route('user/sign-up', methods=['POST']) 
-def user_signup():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        user_name = request.form.get('userName')
-        name = request.form.get('name')
-        biography = request.form.get('biography')
-        volunteer_interest = request.form.get('volunteerInterest')
-
-    existing_org = get_org_from_opportunity_title(volunteer_interest)
-    if existing_org == None:
-        flash("That title does not have an organization associated with it. Please try again")
-    else:
-        v_opportunity = query_org_opportunity(existing_org.__id, volunteer_interest)
-        v_opportunity_submit = {'title': v_opportunity.title,
-               'description': v_opportunity.description,
-               'dateOfRequest': datetime.now().date(),
-               'timeOfRequest': datetime.now().time()
-               }
-        inserted_id = mongo_client.volunteer_connect.user.insert_one({
-            'name': name,
-            'userName': user_name,
-            'email': email,
-            'biography': biography,
-            'preferredContactMethod': 'email',
-            'volunteerInterests': v_opportunity_submit,
-            'status': 'Pending'
-                  }).inserted_id
-        if inserted_id != None:
-            print("inserted id", inserted_id)
-            flash("Successfully Submitted volunteer interest. Your submission is now being reviewed")
-    return render_template('sign_up.html')  
 
 
 
@@ -90,7 +44,7 @@ def register():
 
 @auth.route('/admin/register-opportunity', methods=['POST', 'GET'])
 def volunteer_opportunities():
-    if(request.method == 'POST' and g.admin_requst == True):
+    if request.method == 'POST' and g.admin_requst == True:
         organization_id = request.form.get('organizationId')
         title = request.form.get('title')
         description = request.form.get('description')
@@ -101,7 +55,7 @@ def volunteer_opportunities():
         existing_org_id = ObjectId(organization_id)
         existing_org = mongo_client.volunteer_opportunities.volunteering_options.find_one({"_id": existing_org_id})
 
-        if(existing_org == None):
+        if existing_org == None:
             flash("You cannot add a volunteer option for an organization that is not registered", category='error')
 
         else:
@@ -115,45 +69,13 @@ def volunteer_opportunities():
                     'time': time
                 }
             ).inserted_id
-        if(volunteer_id != None):
+        if volunteer_id != None:
             flash("Submitted Successfully. Thank you for your submittion!", category='success')
-            return redirect(url_for(views.home))
-    else:
+            return redirect(url_for('views.home'))
+    elif g.admin == False:
         render_template('unauthorized.html')
     return render_template("volunteer_opportunities.html")
 
-#helper function section
-def process_opportunities(existing_opportunities):
-    results = []
-    for opportunity_list in existing_opportunities:
-        for json_str_dict in opportunity_list:
-            json_string = json.dumps(json_str_dict)
-            results.extend([json.loads(json_string)])
-    return results
 
-def get_org_from_opportunity_title(opportunity_title):
-    return mongo_client.volunteer_connect.organization.find({"volunteerOpportunities.title":opportunity_title})
-
-def query_org_opportunity(existing_org_id, volunteer_interest):
-    title_to_match = volunteer_interest
-    document_result = mongo_client.volunteer_connect.organization.aggregate([
-        {
-            "$match": {"_id":existing_org_id}
-        },
-        {
-        "$project": {
-            "matchedData": {
-                "$filter": {
-                    "input": "$nestedData",
-                    "as": "item",
-                    "cond": {"$eq": ["$$item.title", title_to_match]}
-                }}
-            }
-        }
-    ])
-    matched_result = list(document_result)[0]["matchedData"]
-    print("matched data found=============================")
-    pprint(matched_result)
-    return matched_result
 
 
--- webapp/routes/dashboard_main.py
+++ webapp/routes/dashboard_main.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, request, render_template, jsonify, get_flashed_messages
+from flask import Blueprint, request, render_template,flash
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
 import pprint
 import json
 import os
+from datetime import datetime
 from pymongo import MongoClient
 
 load_dotenv()
@@ -23,12 +24,75 @@ def dashboard_main():
     all_volunteer_opportunities = process_opportunities(existing_opportunities)
     return render_template('home.html', opportunities=all_volunteer_opportunities)
 
+@dashboard.route('/user/sign-up', methods=['POST', 'GET']) 
+def user_signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        user_name = request.form.get('userName')
+        name = request.form.get('name')
+        biography = request.form.get('biography')
+        volunteer_interest = request.form.get('volunteerInterest')
+
+        existing_org = get_org_from_opportunity_title(volunteer_interest)
+        org_id = extract_document_id(existing_org)
+        matched_opportunity = extract_nested_document(org_id,volunteer_interest)
+        if existing_org == None:
+            flash("That title does not have an organization associated with it. Please try again")
+        else:
+            v_opportunity_submit = {'title': matched_opportunity['title'],
+                'description': matched_opportunity['description'],
+                'dateOfRequest': datetime.now().date().strftime('%Y-%m-%d'),
+                'timeOfRequest': datetime.now().time().strftime('%H:%M:%S')
+                }
+            inserted_id = mongo_client.volunteer_connect.user.insert_one({
+                'name': name,
+                'userName': user_name,
+                'email': email,
+                'biography': biography,
+                'preferredContactMethod': 'email',
+                'volunteerInterests': v_opportunity_submit,
+                'status': 'Pending'
+                    }).inserted_id
+            if inserted_id != None:
+                print("inserted id", inserted_id)
+                flash("Successfully Submitted volunteer interest. Your submission is now being reviewed")
+    return render_template('sign_up.html')  
+
+
+ #helper function section
 def process_opportunities(existing_opportunities):
     results = []
     for opportunity_list in existing_opportunities:
         for json_str_dict in opportunity_list:
             json_string = json.dumps(json_str_dict)
             results.extend([json.loads(json_string)])
-    #pprint.pprint(results)
     return results
-    
+
+def get_org_from_opportunity_title(opportunity_title):
+    return mongo_client.volunteer_connect.organization.find({"volunteerOpportunities.title":opportunity_title})
+
+def extract_document_id(existing_org):
+      for document in existing_org:
+          org_id = document['_id']
+      return org_id
+
+def extract_nested_document(org_id, title_to_match):
+    org_query = {"_id": ObjectId(org_id)}
+    org = mongo_client.volunteer_connect.organization.find_one(org_query)
+    nested_documents = org.get("volunteerOpportunities",[])
+    filtered_nested_doc = next((v_opportunity for v_opportunity in nested_documents if v_opportunity.get("title") == title_to_match), None)
+    return filtered_nested_doc
+        
+
+"""def query_org_opportunity(existing_org_id, volunteer_interest):
+    title_to_match = volunteer_interest
+    org_id = ObjectId(existing_org_id)
+    query = {
+        "_id": org_id,
+        "volunteerOpportunities.title": title_to_match
+    }
+    org_opportunity = mongo_client.volunteer_connect.organization.find_one(query)
+    print("matched data found=============================")
+    print(org_opportunity)
+    #print("matched results:::::", str(matched_result))
+    return """
