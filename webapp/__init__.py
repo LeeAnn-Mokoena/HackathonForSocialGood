2b8118b982c56829a1417d7c50a68dc027520a57
LeeAnn-Mokoena <leeann.mokoena@gmail.com>
2023-10-24 21:35:10 -0400

    cleanup

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -5,18 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def create_app():
-    #app instance
     app = Flask(__name__)
     app.config['SECRET_KEY']  = os.getenv("SECRET_KEY")
 
-    from .routes.auth import auth as user_auth, main
-    from .routes.organizations import v_opportunities
+    from .routes.auth import auth , main
     from .routes.dashboard_main import dashboard
     
-    app.register_blueprint(user_auth)
+    app.register_blueprint(auth)
     app.register_blueprint(main)
     app.register_blueprint(dashboard)
-    app.register_blueprint(v_opportunities, url_prefix='/')
-
-    print('app started::')
     return app
--- webapp/routes/auth.py
+++ webapp/routes/auth.py
@@ -5,11 +5,8 @@ from dotenv import load_dotenv
 from passageidentity import Passage, PassageError
 from bson.objectid import ObjectId
 
-
 load_dotenv()
-
 auth = Blueprint('auth', __name__)
-main = Blueprint('main', __name__)
 
 mongo_client = MongoClient(os.getenv("MONGO_URI"))
 API_URL = os.getenv("API_URL")
@@ -33,48 +30,81 @@ def before_request():
     except PassageError:
         return render_template('unauthorized.html')
 
-
-
-
 @auth.route('/register')
 def register():
     return render_template('register.html', psg_app_id=PASSAGE_APP_ID)
 
-
-@auth.route('/admin/register-opportunity', methods=['POST', 'GET'])
-def volunteer_opportunities():
-    if request.method == 'POST' and g.admin_requst == True:
-        organization_id = request.form.get('organizationId')
-        title = request.form.get('title')
-        description = request.form.get('description')
-        location = request.form.get('location')
-        date = request.form.get('date')
-        time = request.form.get('time')
-
-        existing_org_id = ObjectId(organization_id)
-        existing_org = mongo_client.volunteer_opportunities.volunteering_options.find_one({"_id": existing_org_id})
-
-        if existing_org == None:
-            flash("You cannot add a volunteer option for an organization that is not registered", category='error')
-
+@auth.route('/admin/register-org', methods=['POST', 'GET'])
+def register_organization():
+    if request.method == 'POST' and g.admin_request == True:
+        organization_name = request.form.get('org-name')
+        existing_org = get_org_from_name(organization_name)
+        if existing_org != None:
+            flash("Organization already exists. Update the organization instead")
+            redirect(url_for('organizations.create_organization'))
         else:
-            volunteer_id = mongo_client.volunteer_opportunities.volunteering_options.insert_one(
-                {
-                    'organization_id': organization_id,
-                    'title': title,
-                    'description': description,
+            org_bio = request.form.get('description')
+            location = request.form.get('location')
+            contact_info = request.form.get('contactInformation')
+            volunteer_opp_title = request.form.get('opportunityTitle')
+            volunteer_opp_description = request.form.get('opportunityDescr')
+            date = request.form.get('date')
+            start_time = request.form.get('startTime')
+            end_time = request.form.get('endTime')
+            location_type = request.form.get('locationType')
+
+            volunteer_opportunity = [{
+                'title': volunteer_opp_title,
+                'description': volunteer_opp_description,
+                'date': date,
+                'startTime': start_time,
+                'end_Time': end_time,
+                'locationType': location_type
+            }]
+
+            existing_org = mongo_client.volunteer_opportunities.volunteering_options.find_one({"name": organization_name})
+            if existing_org != None:
+                flash("Organization with the same name already exists. Organization name needs to be different to avoid confusion")
+            else:
+                inserted_id = mongo_client.volunteer_connect.organization.insert_one({
+                    'name': organization_name,
+                    'description': org_bio,
                     'location': location,
-                    'date': date,
-                    'time': time
-                }
-            ).inserted_id
-        if volunteer_id != None:
-            flash("Submitted Successfully. Thank you for your submittion!", category='success')
-            return redirect(url_for('views.home'))
+                    'contactInformation': contact_info,
+                    'volunteerOpportunities': volunteer_opportunity
+                })
+                print("inserted id/////////", inserted_id)
+
+            
+                return redirect(url_for('views.home'))
     elif g.admin == False:
         render_template('unauthorized.html')
-    return render_template("volunteer_opportunities.html")
+    return render_template("organization_register.html")
+
+@auth.route('/admin/remove_org')
+def remove_organization():
+    org_name = request.form.get('org-name')
+    org_name_confirm = request.form.get('org-name-confirm')
+    if org_name != org_name_confirm:
+        flash("Organization names don't match. Try again")
+        redirect(url_for('remove_organization.html'))
+    else:
+        existing_org = get_org_from_name(org_name)
+        if existing_org == None:
+            flash("No organization matching that name was returned", category='error')
+        else:
+            result = mongo_client.volunteer_connect.organization.delete_one({'name': org_name})
+            if result > 0:
+                flash('Successfully removed organization', category='success')
+    return render_template('remove_organization.html')
+
+@auth.route('/organizations', methods=['GET'])
+def get_organizations():
+    organizations_list = mongo_client.volunteer_connect.organization.find()
+    print("organization list:::", organizations_list)
+    return render_template('organization.html',organizations=organizations_list )
 
 
-
-
+#helper_function
+def get_org_from_name(org_name):
+    return mongo_client.volunteer_connect.organization.find({"name":org_name})
